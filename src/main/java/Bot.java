5a2092c334fbd1f8cfad581019cecb5f699781e8
uchitsa <uchitsa@ya.ru>
2023-01-18 01:32:33 +0300

    update bot

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -3,11 +3,14 @@ import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.CallbackQuery;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
 import com.pengrad.telegrambot.request.BaseRequest;
 import com.pengrad.telegrambot.request.SendMessage;
 
 public class Bot {
 
+    private static final String IN_PROGRESS_LABEL = "...";
     private final TelegramBot bot = new TelegramBot(System.getenv("BOT_TOKEN"));
 
     public void serve() {
@@ -25,7 +28,21 @@ public class Bot {
 
         BaseRequest request = null;
 
-        if (message != null) {
+        if (message != null && message.viaBot() != null && message.viaBot().username().equals("figaGame_bot")) {
+            InlineKeyboardMarkup replyMarkup = message.replyMarkup();
+            if (replyMarkup == null) return;
+
+            InlineKeyboardButton[][] buttons = replyMarkup.inlineKeyboard();
+
+            if (buttons == null) return;
+
+            InlineKeyboardButton button = buttons[0][0];
+            String buttonLabel = button.text();
+
+            if (!buttonLabel.equals(IN_PROGRESS_LABEL)) return;
+
+            //TODO
+
             Long chatID = message.chat().id();
             request = new SendMessage(chatID, "Hello!");
         }
